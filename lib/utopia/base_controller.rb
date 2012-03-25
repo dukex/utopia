98914b93b2a219046b0e3a681f2404310498053d
Duke <duke@riseup.net>
2012-03-25 05:08:19 -0300

    updated files to 2 space

--- lib/utopia/base_controller.rb
+++ lib/utopia/base_controller.rb
@@ -1,8 +1,8 @@
 require 'inherited_resources'
 
 module Utopia
-	class BaseController < ::InheritedResources::Base
-		actions :index, :show
-		respond_to :xml, :json#, :cvs
-	end
+  class BaseController < ::InheritedResources::Base
+    actions :index, :show
+    respond_to :xml, :json#, :cvs
+  end
 end
