918553d876220adb291cc5a5db51a7527d6e7314
Duke <duke@riseup.net>
2012-04-12 19:06:45 -0300

    added GPL disclaimer on utopia files

--- app/assets/javascripts/utopia/application.js
+++ app/assets/javascripts/utopia/application.js
@@ -1,15 +1,20 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+//--
+// Copyright (C) 2012 Emerson Vinicius Moreira de Almeida
+
+// This file is part of Utopia source code.
+
+// Utopia source code is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the License,
+// or (at your option) any later version.
+
+// Utopia source code is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Foobar; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//++
