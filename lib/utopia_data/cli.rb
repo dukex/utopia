b1a92d96776669f7adb7cf945406d219855d4e72
Duke <duke@vertigem.xxx>
2013-10-19 00:43:57 -0300

    updated templates

--- lib/utopia_data/cli.rb
+++ lib/utopia_data/cli.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'active_support/inflector'
 
 module UtopiaData
   class CLI < Thor
@@ -10,7 +11,15 @@ module UtopiaData
 
     desc "new project_name", "Create a new utopia"
     def new(name)
-      directory('templates', name, verbose: true)
+      @name = name
+      directory('templates/app', name, verbose: true)
+      template('templates/app.erb', "#{name}/#{name}_app.rb")
+      template('templates/config.ru.erb', "#{name}/config.ru")
+    end
+
+    private
+    def name
+      @name
     end
   end
 end
